059607736290239471fe7dc03e8f4fdc3e714356
Sun Ah Lee <sunahlee@blaze.Millennium.Berkeley.EDU>
2019-12-03 23:04:33 -0800

    modified for CommEfficient opt

--- topHCS/topHCS.py
+++ topHCS/topHCS.py
@@ -17,7 +17,7 @@ def topk(vec, k):
 class TopHCS(object):
 	""" Represents one worker"""
 	def __init__(self, d, c, r, h, numBlocks, device='cpu'): 
-		self.h, self.d = h, d
+		self.h, self.d = h, d 
 		self.device = device
 		self.topH = torch.zeros(d, dtype=torch.float, device=self.device)
 		self.csvec = CSVec(d=d, c=c, r=r, numBlocks=numBlocks, device=self.device)
@@ -32,29 +32,33 @@ class TopHCS(object):
 		""" Compresses vector """
 		""" Save top-h elements in self.topH, sketch bottom d-h elements """
 		""" csvec and topH should be zero before storing """
-		assert(self.topH.nonzero().numel() == 0)
+		# assert(self.topH.nonzero().numel() == 0)
+                # changed this for commefficient optimizer
 		self.topH = topk(vec, self.h).to(self.device)
-		self.csvec += (vec - self.topH).to(self.device)
+		self.csvec.accumulateVec((vec - self.topH).to(self.device))
         
-        # TODO: is the logic right?
 	def accumulateTable(self, table):
-		if table.size() != self.table.size():
+		if table.size() != self.csvec.table.size():
 			msg = "Passed in table has size {}, expecting {}"
-			raise ValueError(msg.format(table.size(), self.table.size()))
+			raise ValueError(msg.format(table.size(), self.csvec.table.size()))
 		else:
-			self.table += table
+			self.csvec.accumulateTable(table)
 
 	@classmethod
 	def topKSum(cls, workers, k, unSketchNum=0):
+		assert isinstance(workers, list), "workers must be a list"
 		sketchSum = copy.deepcopy(workers[0].csvec) 
 		sketchSum.zero()
 		topHSum = torch.zeros_like(workers[0].topH)
 		for w in workers:
-			sketchSum += w.csvec
+			sketchSum.accumulateTable(w.csvec.table)
 			topHSum += w.topH
 		d = len(topHSum)
 		unSketchNum = d if (unSketchNum == 0) else unSketchNum
 		unSketchedSum = sketchSum.unSketch(k=unSketchNum) 
+		if topHSum.size() != unSketchedSum.size():
+			msg = "topHSum has size {}, unSketchedSum size {}"
+			raise ValueError(msg.format(topHSum.size(), unSketchedSum.size()))
 		ret = topk(topHSum + unSketchedSum, k)
 		return ret
 
